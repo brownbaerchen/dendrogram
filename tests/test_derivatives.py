8c5d5cba2f043151b0920f8d92640ab3bb1a11e9
Thomas Baumann <39156931+brownbaerchen@users.noreply.github.com>
2026-02-10 14:49:40 +0100

    Fix linter

--- tests/test_derivatives.py
+++ tests/test_derivatives.py
@@ -1,5 +1,6 @@
 import pytest
 
+
 def test_derivative_2D():
     import numpy as np
     from dendro.derivative import compute_derivative
@@ -7,29 +8,29 @@ def test_derivative_2D():
     x = np.linspace(0, 1, 64)
     y = np.linspace(0, 1, 2)
 
-    X, Y = np.meshgrid(x, y, indexing='ij')
+    X, Y = np.meshgrid(x, y, indexing="ij")
 
     data = X
     expect_derivative = np.ones_like(data)
     derivative = compute_derivative([X, Y], data, axis=0)
     assert np.allclose(expect_derivative, derivative)
-    
+
     data = Y
     expect_derivative = np.ones_like(data)
     derivative = compute_derivative([X, Y], data, axis=1)
     assert np.allclose(expect_derivative, derivative)
 
-    data = X+Y
+    data = X + Y
     expect_derivative = np.ones_like(data)
     derivative = compute_derivative([X, Y], data, axis=1)
     assert np.allclose(expect_derivative, derivative)
 
-    data = X*Y
+    data = X * Y
     expect_derivative = X.copy()
     derivative = compute_derivative([X, Y], data, axis=1)
     assert np.allclose(expect_derivative, derivative)
 
-    data = X*Y
+    data = X * Y
     expect_derivative = Y.copy()
     derivative = compute_derivative([X, Y], data, axis=0)
     assert np.allclose(expect_derivative, derivative)
@@ -45,20 +46,19 @@ def test_derivative_2D():
     assert np.allclose(expect_derivative[1:-1, :], derivative[1:-1, :])
 
 
-@pytest.mark.parametrize('f', [1, 2, 4])
+@pytest.mark.parametrize("f", [1, 2, 4])
 def test_find_extrema(f):
     import numpy as np
     from dendro.derivative import find_extrema, compute_derivative
 
-    x = np.linspace(0, 2*np.pi, 256)
-    data = np.sin(f*x)**2
+    x = np.linspace(0, 2 * np.pi, 256)
+    data = np.sin(f * x) ** 2
     deriv = compute_derivative([x], data)
 
     extrema = find_extrema(deriv)
-    expect_extrema = ((f*x) % (np.pi/2)) <= 1.8*x[1]
+    expect_extrema = ((f * x) % (np.pi / 2)) <= 1.8 * x[1]
     assert np.allclose(expect_extrema, expect_extrema)
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     test_derivative_2D()
