fa36c059a14765002e80d95372e8800a729be57f
Thomas Baumann <39156931+brownbaerchen@users.noreply.github.com>
2026-03-19 16:19:42 +0100

    Linting

--- src/dendro/vertical_split.py
+++ src/dendro/vertical_split.py
@@ -1,6 +1,7 @@
 import numpy as np
 import heat as ht
 
+
 def _get_local_indices(flat_data, ntasks, halo_size):
     assert len(flat_data.shape) == 1
 
@@ -24,19 +25,23 @@ def _get_local_indices(flat_data, ntasks, halo_size):
     local_idx = [idx[s] for s in local_slices]
     return local_idx
 
+
 def _prepare_data_for_distribution(data):
     if isinstance(data, ht.DNDarray):
         import warnings
-        warnings.warn(f'Converting data to numpy in vertical distribution')
+
+        warnings.warn(f"Converting data to numpy in vertical distribution")
         data = data.numpy()
 
     return data.flatten()
+
 
 def _get_local_data(flat_data, shape, local_idx):
     local_data = np.empty_like(flat_data)
     local_data[...] = np.nan
     local_data[local_idx] = flat_data[local_idx]
     return local_data.reshape(shape)
+
 
 def distribute_vertically_serial(data, ntasks, halo_size):
     flat_data = _prepare_data_for_distribution(data)
--- tests/test_vertical_splitting.py
+++ tests/test_vertical_splitting.py
@@ -2,10 +2,11 @@ import pytest
 import numpy as np
 import heat as ht
 
-@pytest.mark.parametrize('n', [16, 17])
-@pytest.mark.parametrize('ndim', [1, 2, 3])
-@pytest.mark.parametrize('ntasks', [1, 2, 3])
-@pytest.mark.parametrize('halo_size', [0, 1, 2])
+
+@pytest.mark.parametrize("n", [16, 17])
+@pytest.mark.parametrize("ndim", [1, 2, 3])
+@pytest.mark.parametrize("ntasks", [1, 2, 3])
+@pytest.mark.parametrize("halo_size", [0, 1, 2])
 def test_vertical_distribution_serial(n, ndim, ntasks, halo_size):
     from dendro.vertical_split import distribute_vertically_serial
 
@@ -21,10 +22,14 @@ def test_vertical_distribution_serial(n, ndim, ntasks, halo_size):
     assert ht.allclose(global_data, data)
 
     # check that data is sorted by size
-    min_vals = [np.min(local_data[i][np.isfinite(local_data[i])]) for i in range(ntasks)]
-    max_vals = [np.max(local_data[i][np.isfinite(local_data[i])]) for i in range(ntasks)]
-    assert all([max_vals[i] < max_vals[i+1] for i in range(ntasks-1)]), max_vals
-    assert all([min_vals[i] < min_vals[i+1] for i in range(ntasks-1)]), min_vals
+    min_vals = [
+        np.min(local_data[i][np.isfinite(local_data[i])]) for i in range(ntasks)
+    ]
+    max_vals = [
+        np.max(local_data[i][np.isfinite(local_data[i])]) for i in range(ntasks)
+    ]
+    assert all([max_vals[i] < max_vals[i + 1] for i in range(ntasks - 1)]), max_vals
+    assert all([min_vals[i] < min_vals[i + 1] for i in range(ntasks - 1)]), min_vals
 
     # check that we distributed the data
     nvals = [local_data[i][np.isfinite(local_data[i])].size for i in range(ntasks)]
@@ -34,5 +39,6 @@ def test_vertical_distribution_serial(n, ndim, ntasks, halo_size):
         assert all([n < data.size for n in nvals]), nvals
         assert sum(nvals) >= data.size
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     test_vertical_distribution_serial(16, 2, 4, 4)
