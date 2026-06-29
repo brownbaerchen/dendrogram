d413dfd28e674c89bc2e8352ac82aec6face54a4
Thomas Baumann <39156931+brownbaerchen@users.noreply.github.com>
2026-06-29 18:12:46 +0200

    Add one more change

--- src/dendro/utils.py
+++ src/dendro/utils.py
@@ -14,16 +14,20 @@ def get_1d_data(n):
     return x, data
 
 
-def get_2d_data(n):
+def get_2d_data(n, n_peaks=-1):
     x = ht.linspace(0, 1, n)
     y = ht.linspace(0, 1, n)
     X, Y = ht.meshgrid(x, y, indexing="ij")
+    X.resplit_(0)
+    Y.resplit_(0)
 
-    peaks = [(0.25, 0.25), (0.47, 0.4), (0.65, 0.65), (0.3, 0.7)]
-    heights = [0.2, 0.65, 0.6, 0.7, 0.65]
+    peaks = [(0.47, 0.4), (0.65, 0.65), (0.3, 0.7), (0.25, 0.25)]
+    heights = [0.65, 0.6, 0.7, 0.65, 0.2]
+
+    n_peaks = len(peaks) if n_peaks < 0 else n_peaks
 
     data = ht.zeros_like(X)
-    for peak, height in zip(peaks, heights):
-        data += height * ht.exp(-((X - peak[0]) ** 2 + (Y - peak[1]) ** 2) / 0.03)
+    for i in range(n_peaks):
+        data += heights[i] * ht.exp(-((X - peaks[i][0]) ** 2 + (Y - peaks[i][1]) ** 2) / 0.03)
 
     return X, Y, data
