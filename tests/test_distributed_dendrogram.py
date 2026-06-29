c8fde3edf5d0a917fefd5c113e6de092015a5c9e
Thomas Baumann <39156931+brownbaerchen@users.noreply.github.com>
2026-06-29 18:14:06 +0200

    Refactor

--- tests/test_distributed_dendrogram.py
+++ tests/test_distributed_dendrogram.py
@@ -10,7 +10,6 @@ def test_2D():
 def test_1D():
     import heat as ht
     import numpy as np
-    import matplotlib.pyplot as plt
     from astrodendro.dendrogram import Dendrogram
     from astrodendro.structure import Structure
 
@@ -18,32 +17,13 @@ def test_1D():
     x = x.numpy()
     data = data.numpy()
 
-    plt.plot(x, data, color="black")
-
     reference_dendrogram = Dendrogram.compute(data)
 
-    # %% [markdown]
-    # Next, we are going to split up the data into chunks that we will compute the dendrograms independently on.
-
-    # %%
     ntasks = 2
     elements_per_task = data.shape[0] // ntasks
     local_slices = [
         slice(i * elements_per_task, (i + 1) * elements_per_task) for i in range(ntasks)
     ]
-
-
-    plt.plot(x, data, color="black")
-    for i, s in enumerate(local_slices):
-        marker = "o" if i % 2 == 0 else "x"
-        plt.scatter(x[s], data[s], marker=marker, label=f"Data on task {i}")
-    plt.legend(frameon=False)
-
-    # %% [markdown]
-    # Next, we simply compute dendrograms on the local data.
-    # After we have computed them, we add the shift from local data to global data.
-
-    # %%
 
     local_dendrograms = [Dendrogram.compute(np.array(data[s])) for s in local_slices]
 
@@ -68,27 +48,14 @@ def test_1D():
     chunks = DistributedDendrogram.chunk_local_structures(indices, values, local_extrema)
     chunks = DistributedDendrogram.sort_chunks(chunks, data)
 
-    print(f'Split the global dendrogram with {data.shape[0]} data points into {len(chunks)} chunks')
-
-    # %% [markdown]
-    # Looks about right: The chunks are smaller than the structures and no chunk is part of two structures.
-    # We can actually do a rigorous test that this the chunking satisfies this and also covers all data.
-
-    # %%
     assert np.allclose(np.sort(np.concatenate(chunks)), np.arange(data.shape[0]))
     for structure in reference_dendrogram.all_structures:
         for chunk in chunks:
             if np.any(np.isin(chunk, structure._indices)):
                 assert np.all(np.isin(chunk, structure._indices))
-    # %%
-    chunkA = chunks[0]
-    chunkB = chunks[1]
-    chunkC = chunks[2]
-
     assert not DistributedDendrogram.is_adjacent(chunks[0], chunks[1])
     assert DistributedDendrogram.is_adjacent(chunks[0], chunks[2])
 
-    # %%
     merged_dendrogram = DistributedDendrogram.merge_chunks(chunks, data)
 
     for structure in reference_dendrogram.all_structures:
