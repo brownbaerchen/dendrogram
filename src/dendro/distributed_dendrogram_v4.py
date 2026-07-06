5f105a8e2a889e651c1c5146801ceda2539cc349
Thomas Baumann <39156931+brownbaerchen@users.noreply.github.com>
2026-07-06 15:33:26 +0200

    V4: use torch instead of numpy

--- src/dendro/distributed_dendrogram_v4.py
+++ src/dendro/distributed_dendrogram_v4.py
@@ -1,21 +1,51 @@
 import heat as ht
+import torch
 import numpy as np
 from time import perf_counter
 import logging
 
 from astrodendro.dendrogram import Dendrogram
+from astrodendro.structure import Structure as astrodendro_structure
 
-from dendro.distributed_dendrogram import Structure
+
+class TorchStructure(astrodendro_structure):
+    def __init__(self, indices, values, children=[], idx=None, dendrogram=None):
+
+        self._dendrogram = dendrogram
+        self.parent = None
+        self.children = children
+
+        # Make sure that the children have a reference to the present structure
+        for child in children:
+            child.parent = self
+
+        if np.isscalar(values):
+            self._indices = [indices]
+            self._values = [values]
+
+        if not isinstance(indices, torch.Tensor):
+            indices = torch.tensor(indices)
+        if not isinstance(values, torch.Tensor):
+            values = torch.tensor(values)
+
+        self._indices = indices
+        self._values = values
+        self.idx = idx
+
+        self._vmin, self._vmax = torch.min(values), torch.max(values)
+        if idx >= 0:
+            self._smallest_index = torch.min(self._indices)
+            self._reset_cache()
 
 
-class DistributedDendrogramV3(Dendrogram):
+class DistributedDendrogramV4(Dendrogram):
     logger = logging.getLogger("Dendrogram")
 
     @staticmethod
     def compute(data, **kwargs):
         assert isinstance(data, ht.DNDarray)
 
-        self = DistributedDendrogramV3()
+        self = DistributedDendrogramV4()
         self.data = data
         self.comm = data.comm
 
@@ -40,10 +70,10 @@ class DistributedDendrogramV3(Dendrogram):
 
         # add offsets to local indices
         _, offsets = data.counts_displs()
-        offset = np.zeros((1, data.ndim), dtype=int)
+        offset = torch.zeros((1, data.ndim), dtype=int)
         offset[:, data.split] = offsets[comm.rank]
         for structure in local_dendrogram.all_structures:
-            structure._indices = np.array(structure._indices) + offset
+            structure._indices = torch.tensor(structure._indices) + offset
 
         return local_dendrogram
 
@@ -63,7 +93,9 @@ class DistributedDendrogramV3(Dendrogram):
         for i, _data in enumerate(all_raw_data):
             if i != self.comm.rank:
                 for me in _data:
-                    structures += [Structure(idx=me[0], indices=me[1], values=me[2])]
+                    structures += [
+                        TorchStructure(idx=me[0], indices=me[1], values=me[2])
+                    ]
 
         return structures
 
@@ -81,15 +113,15 @@ class DistributedDendrogramV3(Dendrogram):
 
         for i, dendrogram in enumerate(local_dendrograms):
             for structure in dendrogram.all_structures:
-                offset = np.zeros((1, data.ndim), int)
+                offset = torch.zeros((1, data.ndim), dtype=torch.int)
                 offset[:, 0] = local_slices[i].start
-                structure._indices = np.array(structure._indices) + offset
+                structure._indices = torch.tensor(structure._indices) + offset
 
         return local_dendrograms
 
     @staticmethod
     def compute_pseudo_parallel(data, ntasks):
-        self = DistributedDendrogramV3()
+        self = DistributedDendrogramV4()
         self.data = data
 
         local_dendrograms = self.compute_local_dendrogram_pseudo_parallel(
@@ -118,14 +150,14 @@ class DistributedDendrogramV3(Dendrogram):
 
     def split_structure(self, structure, split_at, structures):
 
-        if not isinstance(structure._values, np.ndarray):
-            structure._values = np.array(structure._values)
-        if not isinstance(structure._indices, np.ndarray):
-            structure._indices = np.array(structure._indices)
+        if not isinstance(structure._values, torch.Tensor):
+            structure._values = torch.tensor(structure._values)
+        if not isinstance(structure._indices, torch.Tensor):
+            structure._indices = torch.tensor(structure._indices)
 
         top_mask = structure._values > split_at
 
-        bottom_part = Structure(
+        bottom_part = TorchStructure(
             indices=structure._indices[~top_mask],
             values=structure._values[~top_mask],
             idx=self.get_uid(),
@@ -134,8 +166,8 @@ class DistributedDendrogramV3(Dendrogram):
 
         structure._indices = structure._indices[top_mask]
         structure._values = structure._values[top_mask]
-        structure._vmin = np.min(structure._values)
-        structure._vmax = np.max(structure._values)
+        structure._vmin = torch.min(structure._values)
+        structure._vmax = torch.max(structure._values)
 
         self.logger.info(
             f"Split structure at {split_at:.2f}. Remaining top part has {len(structure._values)} values between {structure._vmin:.2f} and {structure._vmax:.2f} and {len(structure._children)} children, bottom part has {len(bottom_part._values)} values between {bottom_part._vmin:.2f} to {bottom_part._vmax:.2f}."
@@ -149,8 +181,8 @@ class DistributedDendrogramV3(Dendrogram):
 
     @staticmethod
     def insert_structure_within(structures, insert):
-        vmax = np.array([structure._vmax for structure in structures])
-        insert_at = np.nonzero(vmax < insert._vmax)[0][0]
+        vmax = torch.tensor([structure._vmax for structure in structures])
+        insert_at = torch.nonzero(vmax < insert._vmax, as_tuple=False)[0][0]
         return structures[:insert_at] + [insert] + structures[insert_at:]
 
     @staticmethod
@@ -162,10 +194,10 @@ class DistributedDendrogramV3(Dendrogram):
         elif to_insert._vmax <= structures[-1]._vmax:
             structures.append(to_insert)
         else:
-            structures = DistributedDendrogramV3.insert_structure_within(
+            structures = DistributedDendrogramV4.insert_structure_within(
                 structures, to_insert
             )
-        DistributedDendrogramV3.logger.info(
+        DistributedDendrogramV4.logger.info(
             f"Inserted structure with {len(to_insert._values)} values between {to_insert._vmin:.2f} and {to_insert._vmax:.2f} into list of {len(structures)} remaining structures."
         )
         return structures
@@ -174,7 +206,7 @@ class DistributedDendrogramV3(Dendrogram):
         self, to_merge, merged_structures, adjacent_structures, structures
     ):
         if len(adjacent_structures) == 0:  # create new leaf
-            leaf = Structure(
+            leaf = TorchStructure(
                 indices=to_merge._indices,
                 values=to_merge._values,
                 idx=len(merged_structures),
@@ -201,11 +233,11 @@ class DistributedDendrogramV3(Dendrogram):
                 )
                 structures = self.insert_structure(structures, bottom_part)
 
-            merge_into._indices = np.vstack([merge_into._indices, to_merge._indices])
-            merge_into._values = np.append(merge_into._values, to_merge._values)
+            merge_into._indices = torch.vstack([merge_into._indices, to_merge._indices])
+            merge_into._values = torch.hstack([merge_into._values, to_merge._values])
             merge_into._vmin = min([merge_into._vmin, to_merge._vmin])
             merge_into._vmax = min([merge_into._vmax, to_merge._vmax])
-            merge_into._smallest_index = np.min(merge_into._indices)
+            merge_into._smallest_index = torch.min(merge_into._indices)
             self.index_map[*to_merge._indices.T] = merge_into.idx
             self.logger.info(
                 f"Merged {len(to_merge._values)} values between {to_merge._vmin:.2f} and {to_merge._vmax:.2f} into existing structure, which now has {len(merge_into._values)} values between {merge_into._vmin:.2f} and {merge_into._vmax:.2f}"
@@ -232,7 +264,7 @@ class DistributedDendrogramV3(Dendrogram):
                     structures = self.insert_structure(structures, bottom_part)
                     # TODO: also break apart to_merge?
 
-            branch = Structure(
+            branch = TorchStructure(
                 indices=to_merge._indices,
                 values=to_merge._values,
                 idx=len(merged_structures),
@@ -252,7 +284,9 @@ class DistributedDendrogramV3(Dendrogram):
         )
 
         merged_structures = []
-        self.index_map = -np.ones(np.add(self.data.shape, 1), dtype=np.int32)
+        self.index_map = -torch.ones(
+            tuple(np.add(self.data.shape, 1)), dtype=torch.int32
+        )
 
         structures = self.sort_structures(structures)
 
@@ -302,7 +336,7 @@ class DistributedDendrogramV3(Dendrogram):
     @staticmethod
     def get_adjacent_structure_indices(structure, index_map):
         adjacent = []
-        idx = np.array(structure._indices)
+        idx = torch.tensor(structure._indices)
         for i in range(idx.shape[1]):
             one = np.zeros((1, idx.shape[1]), dtype=int)
             one[:, i] = 1
@@ -313,7 +347,7 @@ class DistributedDendrogramV3(Dendrogram):
     @staticmethod
     def get_adjacent_structures(structure, merged_structures, index_map):
         adjacent_structure_indices = (
-            DistributedDendrogramV3.get_adjacent_structure_indices(structure, index_map)
+            DistributedDendrogramV4.get_adjacent_structure_indices(structure, index_map)
         )
         ancestor_indices = np.unique(
             [merged_structures[i].ancestor.idx for i in adjacent_structure_indices]
---
+++ tests/test_distributed_dendrogram_v4.py
@@ -0,0 +1,73 @@
+import pytest
+
+from astrodendro.dendrogram import Dendrogram
+
+from dendro.distributed_dendrogram_v4 import DistributedDendrogramV4
+from dendro.utils import compare_dendrograms
+
+
+@pytest.mark.parametrize("ntasks", [1, 2, 4])
+@pytest.mark.parametrize("res", [32, 64])
+def test_1D_v4_pseudo_parallel(ntasks, res):
+    from dendro.utils import get_1d_data
+
+    x, data = get_1d_data(res)
+
+    dendrogram = DistributedDendrogramV4.compute_pseudo_parallel(data.numpy(), ntasks)
+    reference_dendrogram = Dendrogram.compute(data.numpy())
+
+    # import matplotlib.pyplot as plt
+    # fig, axs = plt.subplots(2, ntasks)
+    # local_dendrograms = DistributedDendrogramV4.compute_local_dendrogram_pseudo_parallel(data.numpy(), ntasks)
+    # for i, d in enumerate(local_dendrograms):
+    #     plot_astrodendro_leaves(axs[0, i], x.numpy(), data.numpy(), d.trunk)
+    # plot_astrodendro_leaves(axs[1, 0], x.numpy(), data.numpy(), dendrogram.trunk)
+    # plot_astrodendro_leaves(axs[1, 1], x.numpy(), data.numpy(), reference_dendrogram.trunk)
+    # plt.show()
+
+    compare_dendrograms(reference_dendrogram, dendrogram)
+
+
+@pytest.mark.parametrize("ntasks", [1, 2, 4])
+@pytest.mark.parametrize("res", [32, 64])
+@pytest.mark.parametrize("n_peaks", [1, 2, 3, 4])
+def test_2D_v4_pseudo_parallel(ntasks, res, n_peaks):
+    from dendro.utils import get_2d_data
+
+    _, _, data = get_2d_data(res, n_peaks)
+
+    dendrogram = DistributedDendrogramV4.compute_pseudo_parallel(data.numpy(), ntasks)
+    reference_dendrogram = Dendrogram.compute(data.numpy())
+
+    # import matplotlib.pyplot as plt
+    # from dendro.utils import plot_astrodendro_tree_2D
+    # fig, axs = plt.subplots(2, ntasks)
+    # local_dendrograms = DistributedDendrogramV4.compute_local_dendrogram_pseudo_parallel(data.numpy(), ntasks)
+    # for i, d in enumerate(local_dendrograms):
+    #     plot_astrodendro_tree_2D(axs[0, i], d, d.trunk)
+    # plot_astrodendro_tree_2D(axs[1, 0], dendrogram, dendrogram.trunk)
+    # plot_astrodendro_tree_2D(axs[1, 1], reference_dendrogram, reference_dendrogram.trunk)
+    # plt.show()
+
+    compare_dendrograms(reference_dendrogram, dendrogram)
+
+
+@pytest.mark.mpi(ranks=[1, 2])
+@pytest.mark.parametrize("res", [32])
+@pytest.mark.parametrize("n_peaks", [2, 3])
+def test_2D_v4(mpi_ranks, res, n_peaks):
+    from dendro.utils import get_2d_data
+
+    _, _, data = get_2d_data(res, n_peaks)
+
+    dendrogram = DistributedDendrogramV4.compute(data)
+    reference_dendrogram = Dendrogram.compute(data.numpy())
+    compare_dendrograms(reference_dendrogram, dendrogram)
+
+
+if __name__ == "__main__":
+    import logging
+
+    logging.basicConfig(level=logging.INFO)
+    test_1D_v4_pseudo_parallel(2, 128)
+    # test_2D_v4_pseudo_parallel(2, 32, 2)
