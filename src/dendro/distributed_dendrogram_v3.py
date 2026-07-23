03df78ed8be54414bdcf1216ab5e70cb4079f0ff
Thomas Baumann <39156931+brownbaerchen@users.noreply.github.com>
2026-07-23 14:58:05 +0200

    Refactor to the moon!

--- src/dendro/distributed_dendrogram_v3.py
+++ src/dendro/distributed_dendrogram_v3.py
@@ -239,15 +239,6 @@ class DistributedDendrogramV3(Dendrogram):
                     structures = self.insert_structure(structures, bottom_part_child)
                     self.index_map[*bottom_part_child._indices.T] = -1
 
-                elif child._vmin < to_merge._vmin and child._vmax > to_merge._vmin:
-                    split_at = to_merge._vmax
-                    child, bottom_part = self.split_structure(
-                        child, split_at, structures
-                    )
-                    structures = self.insert_structure(structures, bottom_part)
-                    self.index_map[*bottom_part._indices.T] = -1
-                    # TODO: also break apart to_merge?
-
             branch = Structure(
                 indices=to_merge._indices,
                 values=to_merge._values,
